c1c0018267cd70ccb3e1866a8765dcc4ac6dfbe1
Maxi <maxifop96@gmail.com>
2022-08-10 10:34:30 -0300

    Creacion del proyecto

---
+++ src/test/java/com/nocountry/ncc625m/NcC625mApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocountry.ncc625m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcC625mApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
